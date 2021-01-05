814dbac3e0e1c313c4a982c7eb1042376f710654
Yoshikatsu-Koyanagi <soccer211554@i.softbank.jp>
2021-01-05 19:17:01 +0900

    commit

--- src/components/AppPlus.vue
+++ src/components/AppPlus.vue
@@ -8,7 +8,7 @@
 
 <script>
 export default { 
-  props: ["width","height","min","amount","bg_c_2","bg_c_3"],
+  props: ["width","height","max","amount","bg_c_1","bg_c_2"],
     data() {
       return {
         triangle_width_ratio: 0.9,
@@ -17,36 +17,36 @@ export default {
         flame_weight_ratio: 0.01, //キャンバスの横幅に対するボタンの枠の太さの割合
         line_ratio: 0.5, //三角ボタンの高さに対する＋,-の線の長さの割合
         line_weight_ratio: 0.8, //ボタンの枠の太さに対する＋,-の線の太さ
-    }
+      }
   },
   methods: {
-    draw_minus_btn(minus_color){
-      //-ボタンの枠
+    draw_plus_btn(plus_color){
+      //＋ボタンの枠
       this.context.globalCompositeOperation = "destination-out";
       this.context.beginPath();
-      this.context.moveTo(this.tbw*0.5,this.triangle_top_margin+this.triangle_height); 
-      this.context.lineTo(this.triangle_side_margin,this.triangle_top_margin); 
-      this.context.lineTo(this.triangle_side_margin+this.triangle_width,this.triangle_top_margin);
+      this.context.moveTo(this.tbw*0.5,this.triangle_top_margin); 
+      this.context.lineTo(this.triangle_side_margin,this.triangle_top_margin+this.triangle_height); 
+      this.context.lineTo(this.triangle_side_margin+this.triangle_width,this.triangle_top_margin+this.triangle_height);
       this.context.closePath();	
       this.context.fillStyle = "rgba(0,0,0,1)";
       this.context.fill();
-
+      
       this.context.globalCompositeOperation = "source-over";
       this.context.lineWidth = this.flame_weight;
       this.context.strokeStyle = "rgb(0,0,0)"; 
       this.context.stroke();
-      
-      this.context.fillStyle = minus_color;
+      this.context.fillStyle = plus_color;
       this.context.fill();
-   
-      //-ボタンの"-"
+      
+      //＋ボタンの"＋"の横棒
       this.context.strokeStyle = "rgb(0,0,0)"; 
-      this.context.stroke();
-      this.context.fillStyle = "rgba(0,0,0,1)";
-      this.context.fillRect(this.tbw*0.5-this.line_width*0.5,this.triangle_top_margin+this.triangle_height-this.triangle_bar_height,this.line_width,this.line_weight);
-  
+      this.context.fillStyle= "rgba(0,0,0,1)";
+      this.context.fillRect(this.tbw*0.5-this.line_width*0.5,this.triangle_top_margin+this.triangle_bar_height,this.line_width,this.line_weight);
+
+      //＋ボタンの"＋"の縦棒
+      this.context.fillRect(this.tbw*0.5-this.line_weight*0.5,this.triangle_top_margin+this.triangle_bar_height+this.line_weight*0.5-this.line_width*0.5,this.line_weight,this.line_width);
     },
-  
+
   },
   mounted() {
     this.tbw = this.width;
@@ -61,71 +61,71 @@ export default {
     this.line_width = this.triangle_height*this.line_ratio; //＋,-の線の長さ
     this.line_weight = this.flame_weight*this.line_weight_ratio; //＋,-の線の太さ
 
-    let b = this.$refs.canv;
-    this.context = b.getContext('2d');
-
+    let a = this.$refs.canv;
+    this.context = a.getContext('2d');
+    
     this.gradient_b = this.context.createLinearGradient(this.tbw*0.5, 0, this.tbw*0.5, this.tbh);
-    this.gradient_b.addColorStop(0.0 , this.bg_c_2);
-    this.gradient_b.addColorStop(1.0 , this.bg_c_3);
+    this.gradient_b.addColorStop(0.0 , this.bg_c_1);
+    this.gradient_b.addColorStop(1.0 , this.bg_c_2);
     this.context.fillStyle = this.gradient_b;
     this.context.fillRect(0,0,this.tbw,this.tbh);
 
-    
-    //-ボタンを作成
-    let minus_color; //-ボタンの色
+   
+    //+ボタンを作成
+    let plus_color; //＋ボタンの色
     this.gradient = this.context.createLinearGradient(this.tbw*0.5,this.triangle_top_margin,this.tbw*0.5,this.triangle_top_margin+this.triangle_height);
-    this.gradient.addColorStop(0.0, 'rgba(0,0,0,0.05)');
-    this.gradient.addColorStop(0.8 , 'rgba(0,0,0,0.35)');
-    minus_color = this.gradient 
-    this.draw_minus_btn(minus_color);
-    let touch = false;    
+    this.gradient.addColorStop(0.0 , 'rgba(0,0,0,0.35)');
+    this.gradient.addColorStop(0.8 , 'rgba(0,0,0,0.05)');
+    plus_color = this.gradient;
+    this.draw_plus_btn(plus_color); 
+    let touch = false;   
 
-
+    
     //タッチされたとき（スマホ）
-    b.addEventListener('touchstart', () => { 
-        if ( this.min===undefined ? true : this.amount>this.min ){
-          this.$emit("minusclick");
+    a.addEventListener('touchstart', () => { 
+        if (this.max === undefined ? true : this.amount<this.max){
+          this.$emit("plusclick");
         } 
-        minus_color = "rgba(0,0,0,0.4)";
-        this.draw_minus_btn(minus_color);
+        plus_color = "rgba(0,0,0,0.4)";
+        this.draw_plus_btn(plus_color);
         touch = true;
     }, {
       passive: true
     });
 
     //タッチが離れたとき（スマホ）
-    b.ontouchend = (e) => {
-        minus_color = this.gradient;
-        this.draw_minus_btn(minus_color);
+    a.ontouchend = (e) => {
+        plus_color = this.gradient;
+        this.draw_plus_btn(plus_color);
         touch = true;
     }
 
     //クリックが押されたとき
-    b.onmousedown = (e) => {
+    a.onmousedown = (e) => {
       if (touch == false) {
-        if ( this.min===undefined ? true : this.amount>this.min ){
-          this.$emit("minusclick");
+        if (this.max === undefined ? true : this.amount<this.max){
+          this.$emit("plusclick");
         } 
-        minus_color = "rgba(0,0,0,0.4)";
-        this.draw_minus_btn(minus_color);
-      }
+        plus_color = "rgba(0,0,0,0.4)";
+        this.draw_plus_btn(plus_color); 
+      }  
     }
 
     //クリックが離されたとき
-    b.onmouseup = (e) => {
+    a.onmouseup = (e) => {
       if (touch == false) {
-        minus_color = this.gradient;
-        this.draw_minus_btn(minus_color); 
+        plus_color = this.gradient;
+        this.draw_plus_btn(plus_color);
       }
     }
 
     //マウスがキャンバス外に出た時
-    b.onmouseout = (e) => {
+    a.onmouseout = (e) => {
       if (touch == false) {
-        minus_color = this.gradient;
-        this.draw_minus_btn(minus_color);
+        plus_color = this.gradient;
+        this.draw_plus_btn(plus_color);
       }
-    } 
+    }                 
   }
 }
 </script>
